d354708bbc6f22afa3fa9fcd6c38f9ecab39962d
TaniKon <157127935+TaniKon@users.noreply.github.com>
2024-01-26 17:24:02 +0200

    Update Quarter_of_the_year.py

--- Quarter_of_the_year.py
+++ Quarter_of_the_year.py
@@ -11,3 +11,16 @@ def quarter_of(month):
         return 3
     elif month > 9 and month <= 12: 
         return 4
+
+#another solution
+
+# def quarter_of(month):
+#     # your code here
+#     if month in range(1, 4):
+#         return 1
+#     elif month in range(4, 7):
+#         return 2
+#     elif month in range(7, 10):
+#         return 3
+#     elif month in range(10, 13):
+#         return 4
